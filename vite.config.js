fb86e171e004bb48ef5eca105887c77c8f35a3d8
K V L N Kushal <kls2edmentre@gmail.com>
2025-12-03 23:43:53 +0530

    changing deployment state to github pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 
+  base: '/Kiseki-ko/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
